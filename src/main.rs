96f63fd10e7d6679c26d8a48d63a9584f8de58ed
Ruberald <sajidhossain909@gmail.com>
2023-07-27 09:31:15 +0530

    Raylib initializd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("DINO in Rust")
+        .build();
+
+    let i = Image::load_image("assets/sprite.png")
+        .expect("could not load sprite");
+
+    let t = rl
+        .load_texture_from_image(&thread, &i)
+        .expect("could not load texture from image");
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_texture_rec(
+            &t, 
+            Rectangle::new(76.0, 0.0, 88.0, 96.0), 
+            Vector2::new(0.0, 0.0),
+            Color::WHITE,
+            );
+    }
 }
